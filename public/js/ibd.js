cf9fd91067e333fd7dec190a6a5e1b25e1f314aa
jester <jaimebuddy98@gmail.com>
2022-03-31 12:46:36 -0500

    on upgrade, on success

--- public/js/ibd.js
+++ public/js/ibd.js
@@ -3,8 +3,24 @@ let db;
 const request = indexedDB.open('budgeting_tracker', 1);
 
 
+// UPDATE WHENEVER NEEDED 
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_entry', { autoIncrement: true });
+};
 
 
+// WHEN SUCCESSFUL 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+    db = event.target.result;
+  
+    // CHECK IF APP IS ONLINE
+    if (navigator.onLine) {
+        uploadEntry();
+    }
+};
 
-// listen for app coming back online
-window.addEventListener('online', );
+
+// LISTEN FOR APP COMING BACK ONLINE
+window.addEventListener('online', uploadEntry);
